c4d2fb380b0a0dc339a78be5f0fb44bc61c5c8ee
sharukDroid <Sharuk155796@gmail.com>
2020-04-15 21:57:40 +0600

    Add project files.

---
+++ AjaxLinksAndForms/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AjaxLinksAndForms.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.myDate = DateTime.Now.ToString();
+            return View();
+        }
+        public IActionResult AjaxUpdate()
+        {
+            ViewBag.myDate = DateTime.Now.ToString();
+            return PartialView("pv_AjaxUpdate");
+        }
+    }
+}
---
+++ AjaxLinksAndForms/Controllers/JAStudentsController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AjaxLinksAndForms.Data;
+using AjaxLinksAndForms.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AjaxLinksAndForms.Controllers
+{
+    [Produces("application/json")]
+    public class JAStudentsController : Controller
+    {
+
+        SchoolDbContext schoolDbContext;
+        public JAStudentsController(SchoolDbContext _schoolDbContext)
+        {
+            schoolDbContext = _schoolDbContext;     
+        }
+        [HttpGet]
+        public IEnumerable<Student> GetStudents()
+        {
+            var students = schoolDbContext.Students;
+            return students;
+        }
+
+        [HttpGet]
+        public Student GetStudentById(int id)
+        {
+            var student = schoolDbContext.Students.Find(id);
+            return student;
+        }
+
+
+        [HttpPost]
+        public string UpdateStudent(Student student)
+        {
+            if (student.StudentId==0)
+            {
+                schoolDbContext.Add<Student>(student);
+            }
+            else
+            {
+                schoolDbContext.Update<Student>(student);
+            }
+            schoolDbContext.SaveChanges();
+            return "Operation Successful!";
+        }
+
+        [HttpGet]
+        public string DeleteStudent(int id)
+        {
+            var student = schoolDbContext.Students.Find(id);
+            schoolDbContext.Remove<Student>(student);
+            schoolDbContext.SaveChanges();
+            return "Deleted Successfully!";
+        }
+
+    }
+}
